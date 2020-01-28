ae84091c613602bd492b28dff7f8e8c3e76be00b
Camilo Romero <militoromero10@gmail.com>
2020-01-27 21:50:11 -0500

    add find by description and test

--- src/main/java/com/milo/recipes/controllers/IndexController.java
+++ src/main/java/com/milo/recipes/controllers/IndexController.java
@@ -1,13 +1,32 @@
 package com.milo.recipes.controllers;
 
+import com.milo.recipes.model.Category;
+import com.milo.recipes.model.UnitOfMeasure;
+import com.milo.recipes.repository.CategoryRepository;
+import com.milo.recipes.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","index","/index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: "+categoryOptional.get().getId());
+        System.out.println("UOM id is: "+unitOfMeasureOptional.get().getId());
         return "index";
     }
 
