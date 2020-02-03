8904969aff977829ed6d5805636926c79ccc35ae
Camilo Romero <militoromero10@gmail.com>
2020-02-02 22:39:47 -0500

    show and create recipe hmtl

--- src/main/java/com/milo/recipes/controller/RecipeController.java
+++ src/main/java/com/milo/recipes/controller/RecipeController.java
@@ -1,9 +1,12 @@
 package com.milo.recipes.controller;
 
+import com.milo.recipes.command.RecipeCommand;
 import com.milo.recipes.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +22,18 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.getRecipeById(Long.parseLong(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping({"recipe/new"})
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping(  "recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/"+savedCommand.getId();
     }
 }
